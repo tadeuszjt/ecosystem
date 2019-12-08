69405eebebd5339d1cbfe4a4155d13eba2f5da5a
Tadeusz Tomoszek <TadeuszJT@Gmail.com>
2019-12-08 14:16:43 +0000

    predators moving

--- ecosystem.go
+++ ecosystem.go
@@ -3,41 +3,48 @@ package main
 import (
 	"github.com/tadeuszjt/geom/geom32"
 	"github.com/tadeuszjt/gfx"
+	"github.com/tadeuszjt/data"
 	"math/rand"
 )
 
 const (
 	blobsStart       = 10
-	blobsSpawnOdds   = 100
 	blobsCollideDist = 20
-	blobsBreedOdds   = 100
+	blobsBreedOdds   = 60
 	blobsBreedDist   = 40
+	blobsMaxAge      = 2000
 	
 	predsStart       = 3
 	predsInitialFed  = 100
 	predsEatRadius   = 30
+	predsSightRadius = 100
+	predsSpeed       = 0.6
 )
+
+
 
 var (
 	arena = geom.RectCentred(1000, 1000)
 
+	blobsT = data.Table{ &blobs.pos, &blobs.col, &blobs.age }
 	blobs struct {
-		pos []geom.Vec2
-		col []gfx.Colour
-		age []int
+		pos geom.SliceVec2
+		col SliceColour
+		age data.SliceInt
 	}
 	
+	predsT = data.Table{ &preds.ori, &preds.fed }
 	preds struct {
-		ori []geom.Ori2
-		fed []int
+		ori geom.SliceOri2
+		fed data.SliceInt
 	}
 )
 
 func randColour() gfx.Colour {
 	return gfx.Colour{
-		R: rand.Float32(),
-		G: rand.Float32(),
-		B: rand.Float32(),
+		R: rand.Float32() * 0.5,
+		G: rand.Float32() *0.4 + 0.6,
+		B: rand.Float32() * 0.5,
 		A: 1,
 	}
 }
@@ -57,43 +64,30 @@ func blobCollidesWithAny(pos geom.Vec2) bool {
 	return false
 }
 
-func addBlob(pos geom.Vec2, col gfx.Colour) {
-	blobs.pos = append(blobs.pos, pos)
-	blobs.col = append(blobs.col, col)
-	blobs.age = append(blobs.age, 0)
-}
-
 func spawnBlob() {
 	spawnPos := geom.Vec2Rand(arena)
 	if !blobCollidesWithAny(spawnPos) {
-		addBlob(spawnPos, randColour())
+		blobsT.Append(spawnPos, randColour(), 0)
 	}
-}
-
-func killBlob(i int) {
-	end := len(blobs.pos) - 1
-	
-	if i < end {
-		blobs.pos[i] = blobs.pos[end]
-		blobs.col[i] = blobs.col[end]
-		blobs.age[i] = blobs.age[end]
-	}
-	
-	blobs.pos = blobs.pos[:end]
-	blobs.col = blobs.col[:end]
-	blobs.age = blobs.age[:end]
-}
-
-
-func addPred(pos geom.Vec2) {
-	preds.ori = append(preds.ori, geom.Ori2{pos.X, pos.Y, 0})
-	preds.fed = append(preds.fed, predsInitialFed)
 }
 
 func spawnPred() {
-	addPred(geom.Vec2Rand(arena))
+	spawnPos := geom.Vec2Rand(arena)
+	predsT.Append(
+		geom.Ori2{spawnPos.X, spawnPos.Y, 0},
+		predsInitialFed,
+	)
 }
 	
+func blobsInRange(pos geom.Vec2, radius float32) (s []geom.Vec2) {
+	for _, p := range blobs.pos {
+		if p.Minus(pos).Len2() < radius*radius {
+			s = append(s, p)
+		}
+	}
+	
+	return s
+}
 
 func start() {
 	for i := 0; i < blobsStart; i++ {
@@ -106,31 +100,6 @@ func start() {
 }
 
 func update() {
-	if rand.Intn(blobsSpawnOdds) == 0 {
-		spawnBlob()
-	}
-	
-	/* predators move */
-	for i := range preds.ori {
-	}
-	
-	/* predators eat */
-	for i := range preds.ori {
-		pos := preds.ori[i].Vec2()
-		
-		for j := 0; j < len(blobs.pos); j++ {
-			if blobs.pos[j].Minus(pos).Len2() < predsEatRadius*predsEatRadius {
-				killBlob(j)
-				j--
-				preds.fed[i]++
-				
-				if preds.fed[i] > 100 {
-					preds.fed[i] = 100
-				}
-			}
-		}
-	}
-	
 	/* breed blobs */
 	for i, pos := range blobs.pos {
 		if rand.Intn(blobsBreedOdds) == 0 {
@@ -138,11 +107,50 @@ func update() {
 			childPos := pos.Plus(geom.Vec2RandNormal().ScaledBy(dist))
 			
 			if arena.Contains(childPos) && !blobCollidesWithAny(childPos) {
-				addBlob(childPos, blobs.col[i])
+				blobsT.Append(childPos, blobs.col[i], 0)
 			}
 		}
 	}
 	
+	/* blobs die */
+	blobsT.Filter(func(i int) bool {
+		return rand.Intn(blobsMaxAge - blobs.age[i]) != 0
+	})
 	
+	/* predators move */
+	for i := range preds.ori {
+		pos := preds.ori[i].Vec2()
+		blobsSee := blobsInRange(pos, predsSightRadius)
+		
+		var vel geom.Vec2
+		for _, bpos := range blobsSee {
+			toBlob := bpos.Minus(pos)
+			distToBlob := toBlob.Len()
+			
+			add := toBlob.Normal().ScaledBy(predsSightRadius - distToBlob)
+			vel.PlusEquals(add)
+		}
+		
+		preds.ori[i].PlusEquals(
+			vel.Normal().ScaledBy(predsSpeed).Ori2(),
+		)
+	}
 	
+	/* predators eat */
+	for i := range preds.ori {
+		pos := preds.ori[i].Vec2()
+		
+		blobsT.Filter(func(j int) bool {
+			if blobs.pos[j].Minus(pos).Len2() < predsEatRadius*predsEatRadius {
+				preds.fed[i]++
+				if preds.fed[i] > 100 {
+					preds.fed[i] = 100
+				}
+				
+				return false
+			}
+			
+			return true
+		})
+	}
 }
